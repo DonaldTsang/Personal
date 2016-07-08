9e5be147b9180c966a93e022aef33cff0ef119c3
Donald Tsang <brandonknlee@gmail.com>
2016-07-07 20:01:49 -0700

    Add files via upload

--- bishop.py
+++ bishop.py
@@ -291,3 +291,37 @@ def drunken_bishop_unused(password):
 	sha_unused = sha_160 + sha_224
 	sha_finger = chop(sha_unused, 32)
 	return drunken_bishop_merge(sha_finger)
+
+
+################################################################################
+
+
+from random import randint, shuffle
+def password_gen(total, upcase, lowcase, numbers, others = 0, characters = ''):
+	assert isinstance(total, int)
+	assert isinstance(upcase, int)
+	assert isinstance(lowcase, int)
+	assert isinstance(numbers, int)
+	assert isinstance(others, int)
+	assert isinstance(characters, str)
+	assert total >= ( upcase + lowcase + numbers + others )
+	up_char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	low_char = 'abcdefghijklmnopqrstuvwxyz'
+	num_char = '0123456789'
+	characters = '+-_=.:/' if characters == '' else characters
+	characters = characters
+	royale = up_char + low_char + num_char + characters
+	result = ''
+	for i in range(upcase):
+		result += up_char[randint(0, 25)]
+	for i in range(lowcase):
+		result += low_char[randint(0, 25)]
+	for i in range(numbers):
+		result += num_char[randint(0, 9)]
+	for i in range(others):
+		result += characters[randint(0, len(characters)-1)]
+	for i in range(total - upcase - lowcase - numbers - others):
+		result += royale[randint(0, len(royale)-1)]
+	result = list(result)
+	shuffle(result)
+	return ''.join(result)
