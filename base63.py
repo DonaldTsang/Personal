be25d12d92f11e9a49c83bdd59c0d11740992c02
Donald Tsang <brandonknlee@gmail.com>
2016-07-07 19:07:30 -0700

    Add files via upload

--- base63.py
+++ base63.py
@@ -1,114 +1,87 @@
 #!/usr/bin/python
-
-import re
-
-digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_'
-
-# base=63, exp=448 (56 bytes), limit=75
-
 """
 Bytes (or string)
-	v unicode2num --+
-Intger              +--encode
-	v en_block -----+
+	v u2i-----+
+Intger        +--encode
+	v en------+
 Encoded string
-	v de_block -----+
-Integer             +--decode
-	v num2unicode --+
+	v de------+
+Integer       +--decode
+	v i2u-----+
 Bytes string
-
 """
-
+import re
 def chop(string, length):
 	return [string[i:i+length] for i in range(0, len(string), length)]
+class Codex(object):
+	def __init__(self, base, exp, limit, digit):
+		self.base = base
+		self.exp = exp
+		self.limit = limit
+		self.digit = digit
+	def en(integer, self): # encoding integer into text
+		assert isinstance(integer, int), "Error: message not integer"
+		assert 0 <= integer < (2 ** self.exp), "Error: number out of range"
+		if integer == 0:
+			return ''.zfill(self.limit)
+		str=''
+		while integer != 0:
+			integer, char = divmod(integer, self.base)
+			str = (self.digit[char]) + str
+		return str.zfill(self.limit)[::-1]
+	def de(string, self): # decoding text into integer
+		assert isinstance(string, str)
+		assert len(string) <= self.limit, "Error: too long"
+		assert True == bool(re.fullmatch('[0-9A-Za-z_]{1,%d}' % self.limit, string))
+		string = string[::-1].lstrip("0")
+		integer = 0
+		for char in string:
+			integer *= self.base
+			integer += self.digit.index(char)
+		assert integer < (2 ** self.exp), "Error: number error"
+		return integer
+	def u2i(string, self): # encode unicode into integer
+		if isinstance(string, str):
+			string = string.encode('utf-8')
+		assert isinstance(string, bytes), "Error: message not bytes"
+		assert len(string) <= (self.exp // 8), "Error: string too long"
+		result = 0
+		string = string[::-1].lstrip(b'\x00')
+		for char in string:
+			result *= 256
+			result += char
+		return result
+	def i2u(integer, self): # decodeing integer into unicode
+		assert isinstance(integer, int), "Error: number not integer"
+		assert 0 <= integer < (2 ** self.exp), "Error: number out of range"
+		result = []
+		while integer != 0:
+			integer, r = divmod(integer, 256)
+			result += [r]
+		result += ( [0] * ((self.exp // 8)-len(result)) )
+		return bytes(result)
+	def encode(string, self): # encoding unicode into text
+		return Codex.en(Codex.u2i(string, self), self)
+	def decode(string, self): # decoding text into unicode
+		return Codex.i2u(Codex.de(string, self), self)
+	def mess_en(string, self): # encode unicode into text
+		if isinstance(string, str):
+			string = string.encode('utf-8')
+		assert isinstance(string, bytes), "Error: message not bytes"
+		string = chop(string, self.exp // 8)
+		result = ''
+		for steak in string:
+			result += Codex.encode(steak, self)
+		return result
+	def mess_de(string, self): # decode text into unicode
+		assert isinstance(string, str), "Error: message not string"
+		string = chop(string, self.limit)
+		result = b''
+		for steak in string:
+			result += Codex.decode(steak, self)
+		return result
 
-def divmod63(n):
-	result_q, result_r = 0, n
-	while result_r >= 63:
-		b = result_r
-		q = b >> 6
-		r = b & 63
-		result_q += q
-		result_r = q + r
-	return result_q, result_r
-
-def en448(integer):
-	assert isinstance(integer, int)
-	assert isinstance(num, int), "Error: message not integer"
-	assert 0 <= num < (2 ** 448), "Error: number out of range"
-	if integer == 0:
-		return '0'.zfill(75)
-	str=''
-	while integer != 0:
-		integer, char = divmod(integer, 63)
-		str = (digits[char]) + str
-	return str.zfill(75)
-
-def de448(string):
-	assert isinstance(string, str)
-	assert len(string) <= 75, "Error: too long"
-	assert True == bool(re.fullmatch('[0-9A-Za-z_]{1,75}', string))
-	string = string.lstrip("0")
-	num = 0
-	for char in string:
-		num *= 63
-		num += digits.index(char)
-	assert num < (2 ** 448), "Error: number error"
-	return num
-
-################################################################################
-
-def unicode2num(string):
-	assert isinstance(string, bytes), "Error: message not bytes"
-	assert len(string) <= 56, "Error: string too long"
-	result = 0
-	string = string[::-1].lstrip(b'\x00')
-	for char in string:
-		result *= 256
-		result += char
-	return result
-
-def num2unicode(num):
-	assert isinstance(num, int), "Error: number not integer"
-	assert 0 <= num < (2 ** 448), "Error: number out of range"
-	result = []
-	while num != 0:
-		num, r = divmod(num, 256)
-		result += [r]
-	result += ( [0] * (56-len(result)) )
-	return bytes(result)
-
-################################################################################
-
-def encode448(string):
-	return en448(unicode2num(string))
-
-def decode448(string):
-	return num2unicode(de448(string))
-
-################################################################################
-
-def message_encode448(string):
-	assert isinstance(string, bytes), "Error: message not bytes"
-	string = chop(string, 56)
-	result = ''
-	for steak in string:
-		result += encode448(steak)
-	return result
-
-def message_decode(string):
-	assert isinstance(string, str), "Error: message not string"
-	string = chop(string, 75)
-	result = b''
-	for steak in string:
-		result += decode448(steak)
-	return result
-
-def divmod63(x):
-	a, q, r = 0, x, 0
-	while q >= 63:
-		r = q & 63 # r = q % 64
-		q >>= 6 # q = q
-		a += q # a = a + q
-		q += r # q = q + r
-	return a, q # div and mod
+base63 = Codex(63, 448, 75, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_")
+base62 = Codex(62, 256, 43, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+base61 = Codex(61, 160, 27, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy")
+base60 = Codex(60, 112, 19, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwx")
