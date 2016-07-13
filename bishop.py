216c36860edb35c924758604457e5889558c73a5
Donald Tsang <brandonknlee@gmail.com>
2016-07-12 21:07:17 -0700

    Update bishop.py

--- bishop.py
+++ bishop.py
@@ -49,8 +49,7 @@ def bit_pairs(binary):
 class Direction(object):
     """Encode a sense of direction."""
     def __init__(self, dx, dy):
-        self.dx = dx
-        self.dy = dy
+        self.dx, self.dy = dx, dy
 
 NW = Direction(dx=-1, dy=-1)
 NE = Direction(dx=1, dy=-1)
