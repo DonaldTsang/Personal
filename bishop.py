93366cb01a25336526e97561349fda7561bd9f50
Donald Tsang <brandonknlee@gmail.com>
2016-07-12 22:56:18 -0700

    Update bishop.py

--- bishop.py
+++ bishop.py
@@ -11,24 +11,24 @@ COIN_VALUE_STARTING_POSITION, COIN_VALUE_ENDING_POSITION = 15, 16
 BORDER = '+' + '-' * ROOM_DIMENSIONS[0] + '+\n'
 
 def hex_byte_to_binary(hex_byte): # Convert hex byte into a string of bits
-    assert len(hex_byte) == 2
-    return bin(int(hex_byte, 16))[2:].zfill(8)
+	assert len(hex_byte) == 2
+	return bin(int(hex_byte, 16))[2:].zfill(8)
 
 def bit_pairs(binary): # Convert a word into bit pairs little-endian style.
-    def take(n, iterable):
-        "Return first n items of the iterable as a list"
-        return list(itertools.islice(iterable, n))
-    def all_pairs(iterable):
-        while True:
-            pair = take(2, iterable)
-            if not pair: break
-            yield ''.join(pair)
-    return list(all_pairs(iter(binary)))[::-1]
+	def take(n, iterable):
+		"Return first n items of the iterable as a list"
+		return list(itertools.islice(iterable, n))
+	def all_pairs(iterable):
+		while True:
+			pair = take(2, iterable)
+			if not pair: break
+			yield ''.join(pair)
+	return list(all_pairs(iter(binary)))[::-1]
 
 class Direction(object):
-    """Encode a sense of direction."""
-    def __init__(self, dx, dy):
-        self.dx, self.dy = dx, dy
+	"""Encode a sense of direction."""
+	def __init__(self, dx, dy):
+		self.dx, self.dy = dx, dy
 
 NW = Direction(dx=-1, dy=-1)
 NE = Direction(dx=1, dy=-1)
@@ -36,84 +36,84 @@ SW = Direction(dx=-1, dy=1)
 SE = Direction(dx=1, dy=1)
 
 def directions_from_fingerprint(fingerprint):
-    """
-    Convert the fingerprint (16 hex-encoded bytes separated by colons)
-    to steps (one of four directions: NW, NE, SW, SE).
-    """
-    direction_lookup = {
-        '00': NW,
-        '01': NE,
-        '10': SW,
-        '11': SE,
-    }
-    for hex_byte in fingerprint.split(':'):
-        binary = hex_byte_to_binary(hex_byte)
-        # read each bit-pair in each word right-to-left (little endian)
-        for bit_pair in bit_pairs(binary):
-            direction = direction_lookup[bit_pair]
-            yield direction
+	"""
+	Convert the fingerprint (16 hex-encoded bytes separated by colons)
+	to steps (one of four directions: NW, NE, SW, SE).
+	"""
+	direction_lookup = {
+		'00': NW,
+		'01': NE,
+		'10': SW,
+		'11': SE,
+	}
+	for hex_byte in fingerprint.split(':'):
+		binary = hex_byte_to_binary(hex_byte)
+		# read each bit-pair in each word right-to-left (little endian)
+		for bit_pair in bit_pairs(binary):
+			direction = direction_lookup[bit_pair]
+			yield direction
 
 def move(position, direction):
-    """
-    Returns new position given current position and direction to move in.
-    """
-    x, y = position
-    MAX_X = ROOM_DIMENSIONS[0] - 1
-    MAX_Y = ROOM_DIMENSIONS[1] - 1
-    assert 0 <= x <= MAX_X
-    assert 0 <= y <= MAX_Y
-    new_x, new_y = x + direction.dx, y + direction.dy
-    # the drunk bishop is hindered by the wall.
-    new_x = 0 if new_x <= 0 else min(new_x, MAX_X)
-    new_y = 0 if new_y <= 0 else min(new_y, MAX_Y)
-    return new_x, new_y
+	"""
+	Returns new position given current position and direction to move in.
+	"""
+	x, y = position
+	MAX_X = ROOM_DIMENSIONS[0] - 1
+	MAX_Y = ROOM_DIMENSIONS[1] - 1
+	assert 0 <= x <= MAX_X
+	assert 0 <= y <= MAX_Y
+	new_x, new_y = x + direction.dx, y + direction.dy
+	# the drunk bishop is hindered by the wall.
+	new_x = 0 if new_x <= 0 else min(new_x, MAX_X)
+	new_y = 0 if new_y <= 0 else min(new_y, MAX_Y)
+	return new_x, new_y
 
 def stumble_around(fingerprint):
-    room = Counter()
-    position = STARTING_POSITION
-    for direction in directions_from_fingerprint(fingerprint):
-        position = move(position, direction)
-        room[position] += 1  # drop coin
-    # mark start and end positions
-    room[STARTING_POSITION] = COIN_VALUE_STARTING_POSITION
-    room[position] = COIN_VALUE_ENDING_POSITION
-    return room
+	room = Counter()
+	position = STARTING_POSITION
+	for direction in directions_from_fingerprint(fingerprint):
+		position = move(position, direction)
+		room[position] += 1  # drop coin
+	# mark start and end positions
+	room[STARTING_POSITION] = COIN_VALUE_STARTING_POSITION
+	room[position] = COIN_VALUE_ENDING_POSITION
+	return room
 
 def coin(value):
-    """
-    Display the ascii representation of a coin.
-    """
-    return {
-        0: ' ',
-        1: '.',
-        2: 'o',
-        3: '+',
-        4: '=',
-        5: '*',
-        6: 'B',
-        7: 'O',
-        8: 'X',
-        9: '@',
-        10: '%',
-        11: '&',
-        12: '#',
-        13: '/',
-        14: '^',
-        COIN_VALUE_STARTING_POSITION: 'S',
-        COIN_VALUE_ENDING_POSITION: 'E',
-    }.get(value, '!')
+	"""
+	Display the ascii representation of a coin.
+	"""
+	return {
+		0: ' ',
+		1: '.',
+		2: 'o',
+		3: '+',
+		4: '=',
+		5: '*',
+		6: 'B',
+		7: 'O',
+		8: 'X',
+		9: '@',
+		10: '%',
+		11: '&',
+		12: '#',
+		13: '/',
+		14: '^',
+		COIN_VALUE_STARTING_POSITION: 'S',
+		COIN_VALUE_ENDING_POSITION: 'E',
+	}.get(value, '!')
 
 def display_room(room):
-    X, Y = ROOM_DIMENSIONS
-    def room_as_strings():
-        yield BORDER
-        for y in range(Y):
-            yield '|'
-            for x in range(X):
-                yield coin(room[(x,y)])
-            yield '|\n'
-        yield BORDER
-    return ''.join(room_as_strings())
+	X, Y = ROOM_DIMENSIONS
+	def room_as_strings():
+		yield BORDER
+		for y in range(Y):
+			yield '|'
+			for x in range(X):
+				yield coin(room[(x,y)])
+			yield '|\n'
+		yield BORDER
+	return ''.join(room_as_strings())
 
 ################################################################################
 
