141c759c0b25f332ef389a2e1615757d2608f783
Donald Tsang <brandonknlee@gmail.com>
2016-07-12 21:18:45 -0700

    Update bishop.py

--- bishop.py
+++ bishop.py
@@ -19,31 +19,20 @@ COIN_VALUE_STARTING_POSITION, COIN_VALUE_ENDING_POSITION = 15, 16
 
 BORDER = '+' + '-' * ROOM_DIMENSIONS[0] + '+\n'
 
-def hex_byte_to_binary(hex_byte):
-    """
-    Convert a hex byte into a string representation of its bits,
-    e.g. 'd4' => '11010100'
-    """
+def hex_byte_to_binary(hex_byte): # Convert hex byte into a string of bits
     assert len(hex_byte) == 2
-    dec = int(hex_byte, 16)
-    return bin(dec)[2:].zfill(8)
+    return bin(int(hex_byte, 16))[2:].zfill(8)
 
-def bit_pairs(binary):
-    """
-    Convert a word into bit pairs little-endian style.
-    '10100011' => ['11', '00', '10', '10']
-    """
+def bit_pairs(binary): # Convert a word into bit pairs little-endian style.
     def take(n, iterable):
         "Return first n items of the iterable as a list"
         return list(itertools.islice(iterable, n))
     def all_pairs(iterable):
         while True:
             pair = take(2, iterable)
-            if not pair:
-                break
+            if not pair: break
             yield ''.join(pair)
-    pairs = list(all_pairs(iter(binary)))
-    return list(reversed(pairs))
+    return list(reversed(list(all_pairs(iter(binary)))))
 
 class Direction(object):
     """Encode a sense of direction."""
