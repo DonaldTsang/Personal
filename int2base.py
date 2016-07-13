c943c4f52b4584c601a4587868ff59236f5ff99d
Donald Tsang <brandonknlee@gmail.com>
2016-07-13 13:08:33 -0700

    Update int2base.py

--- int2base.py
+++ int2base.py
@@ -3,8 +3,7 @@ from math import ceil, log10
 def posmod(num, den):
 	# Modulo constrained to 0..abs(den)
 	result = num % den
-	if result < 0:
-		result += abs(den)
+	if result < 0: result += abs(den)
 	return result
 
 def int2array(num, base):
@@ -14,11 +13,8 @@ def int2array(num, base):
 	assert isinstance(num, int), "Error: input not a number"
 	assert isinstance(base, int), "Error: base not a number"
 	assert abs(base) >= 2, "Error: base impossible"
-	if num == 0:
-		# return 0
-		return '0'
-	if num < 0 and base > 0:
-		return  ['-'] + int2array(-num, base)
+	if num == 0: return '0'
+	if num < 0 and base > 0: return  ['-'] + int2array(-num, base)
 	converted = []
 	while num != 0:
 		unit = posmod(num, base)
@@ -40,19 +36,11 @@ def int2base(num, base, alph = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkl
 		return(int2radix(num, base))
 	assert abs(base) >= 2, "Error: base impossible"
 	assert ' ' not in alph, "Error: alphabet contains space"
-	if num == 0:
-		# return 0
-		return alph[0]
+	if num == 0: return alph[0]
 	if num < 0 and base > 0:
-		if '-' not in alph:
-			# return negative integer to positive base with minus
-			return  '--' + int2base(-num, base, alph)
-		elif '~' not in alph:
-			# return negative integer to positive base with tilde
-			return  '~~' + int2base(-num, base, alph)
-		else:
-			# return negative integer to positive base with word
-			return  'negative ' + int2base(-num, base, alph)
+		if '-' not in alph: return  '--' + int2base(-num, base, alph)
+		elif '~' not in alph: return  '~~' + int2base(-num, base, alph)
+		else: return  'negative ' + int2base(-num, base, alph)
 	converted = ''
 	while num != 0:
 		unit = posmod(num, base)
@@ -70,11 +58,8 @@ def int2radix(num, base):
 	assert isinstance(num, int), "Error: input not a number"
 	assert isinstance(base, int), "Error: base not a number"
 	assert abs(base) >= 2, "Error: base impossible"
-	if num == 0:
-		# return 0
-		return '0'
-	if num < 0 and base > 0:
-		return  '-' + int2radix(-num, base)
+	if num == 0: return '0'
+	if num < 0 and base > 0: return  '-' + int2radix(-num, base)
 	ceiling = ceil(log10(abs(base)))
 	converted = ""
 	while num != 0:
@@ -102,18 +87,12 @@ def checkDubs(num, base):
 		if last != revtext[tuple]:
 			break
 		tuple = tuple + 1
-	if last != '0' and tuple == length:
-		return "Full num GET"
-	elif last != '0' and tuple == length - 1:
-		return "Half num GET"
-	elif last == '0' and tuple == length - 1 and text[0] == '1':
-			return "Full zero GET"
-	elif last == '0' and tuple == length - 1 and text[0] != '1':
-			return "Half zero GET"
-	elif tuple > 1:
-		return str(tuple) + "-tuple " + str(last) + "-s"
-	else:
-		return "Last digit is " + str(last)
+	if last != '0' and tuple == length: return "Full num GET"
+	elif last != '0' and tuple == length - 1: return "Half num GET"
+	elif last == '0' and tuple == length - 1 and text[0] == '1': return "Full zero GET"
+	elif last == '0' and tuple == length - 1 and text[0] != '1': return "Half zero GET"
+	elif tuple > 1: return str(tuple) + "-tuple " + str(last) + "-s"
+	else: return "Last digit is " + str(last)
 
 def superDubs(num):
 	assert isinstance(num, int), "Error: not a number"
@@ -127,17 +106,14 @@ def checkPali(num, base):
 	assert isinstance(num, int), "Error: not a number"
 	text = int2base(num, base)
 	revtext = text[::-1]
-	if text == revtext:
-		return "full palindrome"
+	if text == revtext: return "full palindrome"
 	length = len(revtext)
 	for i in range(1, length-2): #Since doubles are not pali
 		fliptext = revtext[:-i]
 		revfliptext = fliptext[::-1]
 		if fliptext == revfliptext:
-			if len(fliptext) == length - 1:
-				return "half palindrome"
-			else:
-				return str(length-i) + "-tuple palindrome"
+			if len(fliptext) == length - 1: return "half palindrome"
+			else: return str(length-i) + "-tuple palindrome"
 	return "no palindrome"
 
 def superPali(num):
