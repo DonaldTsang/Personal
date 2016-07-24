271835eb0685c2856f74fa7ab38f595bf2d5f2df
Donald Tsang <brandonknlee@gmail.com>
2016-07-24 00:39:34 -0700

    Update chinese.py

--- chinese.py
+++ chinese.py
@@ -3,10 +3,7 @@ def date(month, day):
 	assert 0 < day <= 30, "day out of bound"
 	char = "一二三四五六七八九十廿卅卌"[month - 1]
 	if day % 10 == 0:
-		alphabet = "初二三"
-		return char + alphabet[day // 10 - 1] + "十"
+		return char + "初二三"[day // 10 - 1] + "十"
 	else:
-		alph_10 = "初十廿"
-		alph_01 = "一二三四五六七八九"
-		key = day // 10
-		return char + alph_10[key] + alph_01[day % 10 - 1]
+		return char + "初十廿"[day // 10] + \
+		"一二三四五六七八九"[day % 10 - 1]
