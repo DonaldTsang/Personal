1273d6d3cfa23ff0645972afc1633197f9b276cb
Donald Tsang <brandonknlee@gmail.com>
2016-08-02 20:47:46 -0700

    Update time.py

--- time.py
+++ time.py
@@ -1,10 +1,11 @@
+from time import time, sleep
 from datetime import datetime
 from math import floor
 
 characters = "0123456789ABCDEFGHIJ"
 
 def current_time_microsecond():
-	now = datetime.now()
+	now = datetime.now().time()
 	mega = 10 ** 6
 	return (now.hour*mega*60*60)+(now.minute*mega*60)+(now.second*mega)+(now.microsecond)
 
@@ -23,11 +24,20 @@ def current_time_special(time, x):
 	hour = (time*(x**1))//c
 	minute = (time*(x**3))//c-hour*(x**2)
 	second = (time*(x**5))//c-hour*(x**4)-minute*(x**2)
-	return str(hour) + ':' + base(minute, x) + ':' + base(second, x)
+	return base(hour, x)[1] + ':' + base(minute, x) + ':' + base(second, x)
 
 def current_time():
 	time = current_time_microsecond()
 	return 'Decimal---' + current_time_special(time, 10) + '\n' + \
 		'Dozenal---' + current_time_special(time, 12) + '\n' + \
 		'HexTime---' + current_time_special(time, 16) + '\n' + \
-		'Vigesimal-' + current_time_special(time, 20) + '\n'
+		'Vigesimal-' + current_time_special(time, 20) 
+
+def clock_time():
+	end_time = time() + 5
+	while time() < end_time:
+		print(current_time(), end='\r\033[3A')
+	print('\033[4B')
+
+if __name__ == '__main__':
+	clock_time()
