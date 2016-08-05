a488ae9bdc3ad494bf03e1c3bbd4a8bd1d26b723
Donald Tsang <brandonknlee@gmail.com>
2016-08-05 16:24:17 -0700

    Update freedom.py

--- freedom.py
+++ freedom.py
@@ -528,32 +528,3 @@ def multiline():
 	return "\n".join(buffer)
 
 ################################################################################
-
-def hex2cjk(x):
-	assert isinstance(x, int)
-	assert 0x0 <= x <= 0xffff
-	if 0x0 <= x <= 0xfff: x += 0x3400
-	elif 0x1000 <= x <= 0x5fff: x += 0x4e00
-	elif 0x6000 <= x <= 0xffff: x += 0x20000
-	return chr(x)
-
-def cjk2hex(x):
-	assert isinstance(x, str); x = ord(x)
-	if 0x3400 <= x <= 0x43ff: x -= 0x3400
-	elif 0x4e00 <= x <= 0x9dff: x-= 0x4e00
-	elif 0x20000 <= x <= 0x29fff: x-= 0x20000
-	else: assert False, "Character not correct"
-	return x
-
-def trans_cjk_en(byte):
-	assert isinstance(byte, bytes)
-	if len(byte) % 2 == 1: byte += b'\x00'
-	result = ''
-	for i in range(0, len(byte), 2): result += hex2cjk(byte[i:i+1])
-	return result
-
-def trans_cjk_de(string):
-	assert isinstance(string, str)
-	result = b''
-	for char in string: result += cjk2hex(char)
-	return result
