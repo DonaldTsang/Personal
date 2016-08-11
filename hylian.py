34d333fa106a3d6af3dd4377145bf1bb39eb6e3a
Donald Tsang <brandonknlee@gmail.com>
2016-08-11 19:26:49 +0800

    Update hylian.py

--- hylian.py
+++ hylian.py
@@ -270,3 +270,77 @@ def daggot(message="", password=[], shape="tri", wrap=True):
 	assert isinstance(wrap, bool), "Wrapping is not True or False"
 	message = multi_decrypt(formats(message, "de", shape, wrap), password)
 	print(message)
+
+################################################################################
+
+from random import shuffle
+
+class Chaocipher:
+	alphabet = " ABCDEFGHIJKLMNOPRSTUVWXYZ.";
+
+	def __init__(self, left, right):
+		self.orig_left = left
+		self.orig_right = right
+		self.reset()
+		self.check()
+
+	def reset(self):
+		self.left = list(self.orig_left.upper())
+		self.right = list(self.orig_right.upper())
+
+	def check(self):
+		left = self.left
+		right = self.right
+		if len(left) != 27:
+			raise Exception("Left side must contain 27 characters");
+		if len(right) != 27:
+			raise Exception("Left side must contain 27 characters");
+		for i in range(27):
+			char = self.alphabet[i]
+			if left.count(char) != 1:
+				raise Exception("Left side missing '%s'" % char)
+			if right.count(char) != 1:
+				raise Exception("Right side missing '%s'" % char)
+
+	def permute(self, idx):
+		# Permute the left
+		# Step 1: Rotate idx to zenith
+		for cnt2 in range(idx):
+			self.left.append(self.left.pop(0))
+		# Step 2 and 3: extract zenith +1
+		char = self.left.pop(1)
+		# Step 4: Insert at nadir
+		self.left.insert(13, char);
+		# Permute the right
+		# Step 1 and 2: rotate idx to zenith-1
+		for cnt2 in range(idx+1):
+			self.right.append(self.right.pop(0))
+		# Step 3 and 4: remove zenith + 2
+		char = self.right.pop(2)
+		# Step 5: insert at nadir
+		self.right.insert(13, char)
+
+	def crypt(self, text, mode):
+		src = list(text)
+		dest = []
+		for cnt in range(len(src)):
+			char = src[cnt]
+			if self.alphabet.find(char) < 0:
+				#print "Ignoring character '%s'" % char
+				continue
+			if mode == "de":
+				idx = self.left.index(char)
+				dest.append(self.right[idx])
+			elif mode == "en":
+				idx = self.right.index(char)
+				dest.append(self.left[idx])
+			if cnt + 1 == len(src):
+				break
+			self.permute(idx)
+		return ''.join(dest)
+
+	def generate():
+		left, right = list(alphabet), list(alphabet)
+		shuffle(left); shuffle(right)
+		print(["".join(left), "".join(right)])
+		return Chaocipher("".join(left), "".join(right))
