c3c83fb4ad48338abce689115becbb1fbbbda77a
Donald Tsang <brandonknlee@gmail.com>
2016-09-01 17:38:03 +0800

    Update primeFrac.py

--- primeFrac.py
+++ primeFrac.py
@@ -1,40 +1,39 @@
-def testDivide(primfac, n, p):
-	counter = 0
-	while (n % p) == 0:
-		counter += 1
-		n //= p
-	if counter != 0: primfac += [(p, counter)]
+def testDiv(primfac, n, p):
+	ctr = 0
+	while n % p == 0:
+		ctr += 1; n //= p
+	if ctr != 0: primfac += [(p, ctr)]
 	return n
 
 def primes(n):
 	primfac = []
-	n = testDivide(primfac, n, 2)
-	n = testDivide(primfac, n, 3)
-	n = testDivide(primfac, n, 5)
+	n = testDiv(primfac, n, 2)
+	n = testDiv(primfac, n, 3)
+	n = testDiv(primfac, n, 5)
 	counter, d = 0, 7
-	while d**2 <= n:
-		n = testDivide(primfac, n, d)
+	while d ** 2 <= n:
+		n = testDiv(primfac, n, d)
 		d += 4
-		if d** 2 > n: break
-		n = testDivide(primfac, n, d)
+		if d ** 2 > n: break
+		n = testDiv(primfac, n, d)
 		d += 2
-		if d** 2 > n: break
-		n = testDivide(primfac, n, d)
+		if d ** 2 > n: break
+		n = testDiv(primfac, n, d)
 		d += 4
-		if d** 2 > n: break
-		n = testDivide(primfac, n, d)
+		if d ** 2 > n: break
+		n = testDiv(primfac, n, d)
 		d += 2
-		if d** 2 > n: break
-		n = testDivide(primfac, n, d)
+		if d ** 2 > n: break
+		n = testDiv(primfac, n, d)
 		d += 4
-		if d** 2 > n: break
-		n = testDivide(primfac, n, d)
+		if d ** 2 > n: break
+		n = testDiv(primfac, n, d)
 		d += 6
-		if d** 2 > n: break
-		n = testDivide(primfac, n, d)
+		if d ** 2 > n: break
+		n = testDiv(primfac, n, d)
 		d += 2
-		if d** 2 > n: break
-		n = testDivide(primfac, n, d)
+		if d ** 2 > n: break
+		n = testDiv(primfac, n, d)
 		d += 6
 	if n > 1: primfac += [(n, 1)]
 	return primfac
